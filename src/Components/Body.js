1ea332dd171d1b5467cc0ae36af3448aaaac888f
saroj-mali <sarojmali6090@gmail.com>
2024-02-05 21:45:11 +0530

    Modified the card and also added select filter option

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,25 +1,48 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 import { swiggy_api_URL } from "../constants";
 import { Link } from "react-router-dom";
 import { filterData } from "../Utils/Helper";
 import useResData from "../Hooks/useResData";
 import { FiSearch } from "react-icons/fi";
+import Select from "react-select";
 
 const Body = () => {
   const [searchText, setSearchText] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [allRestaurants, FilterRes] = useResData(swiggy_api_URL);
   const [filteredRestaurants, setFilteredRestaurants] = useState(null);
+  const [selectedOption, setSelectedOption] = useState([]);
+
   const options = [
-    { value: "option1", label: "Option 1" },
-    { value: "option2", label: "Option 2" },
-    { value: "option3", label: "Option 3" },
+    { value: "4", label: "Above 4 star" },
+    { value: "4.5", label: "Above 4.5 star" },
+    { value: "3.0 km", label: "Under 3.0 km" },
+    { value: "5.0 km", label: "Under  5.0 km" },
+    { value: "true", label: "veg" },
+    { value: "false", label: "non-veg" },
+    { value: "₹200 for two", label: "₹200 for two" },
+    { value: "₹300 for two", label: "₹300 for two" },
+    { value: "₹400 for two", label: "₹400 for two" },
   ];
-  const handleSelectChange = (e) => {
-    // Handle the select box change here
-    console.log("Selected value:", e.target.value);
+
+  const handleChange = (selectedOption) => {
+    setSelectedOption(selectedOption);
+
+    const filteredData = allRestaurants.filter((res) => {
+      console.log(res.info.veg);
+      console.log(selectedOption.value);
+      return (
+        // res.info.avgRating > selectedOption.value ||
+        // res.info.costForTwo == selectedOption.value ||
+        // res.info.sla.lastMileTravelString < selectedOption.value ||
+
+        res.info.veg == selectedOption.value
+      );
+    });
+    console.log(filteredData);
+    setFilteredRestaurants(filteredData);
   };
 
   const searchData = (searchText, restaurants) => {
@@ -41,7 +64,7 @@ const Body = () => {
   if (!allRestaurants) return <Shimmer />;
 
   return (
-    <div className="-">
+    <div className="min-h-screen">
       <div className="flex items-center justify-center  relative -top-[54px]">
         <input
           type="text"
@@ -65,23 +88,19 @@ const Body = () => {
         <label className="mr-2 ml-10 text-xl">
           Filter <span className="text-[#B22126]">by :</span>{" "}
         </label>
-        <select
-          className=" bg-[#B22126] text-white px-4 py-[6px] rounded-sm bg-shadow w-32 text-center leading-tight focus:outline-none focus:shadow-outline font-bold"
-          onChange={handleSelectChange}
-        >
-          {options.map((option) => (
-            <option key={option.value} value={option.value}>
-              {option.label}
-            </option>
-          ))}
-        </select>
+        <Select
+          options={options}
+          value={selectedOption}
+          onChange={handleChange}
+          className="min-w-40 border-2 text-center rounded-md bg-black border-[#B22126]"
+        />
       </div>
       {errorMessage && <div className="error-container">{errorMessage}</div>}
 
       {allRestaurants?.length === 0 && FilterRes?.length === 0 ? (
         <Shimmer />
       ) : (
-        <div className="flex flex-wrap items-center justify-around self-stretch -mt-8">
+        <div className="flex flex-wrap items-center justify-center self-stretch -mt-8">
           {(filteredRestaurants === null ? FilterRes : filteredRestaurants).map(
             (restaurant) => {
               return (
--- src/Components/RestaurantCard.js
+++ src/Components/RestaurantCard.js
@@ -1,5 +1,6 @@
 import { IMG_CDN_URL } from "../constants";
 import { IoMdStar } from "react-icons/io";
+import { LiaDotCircle } from "react-icons/lia";
 
 const RestaurantCard = ({
   cloudinaryImageId,
@@ -9,29 +10,45 @@ const RestaurantCard = ({
   sla,
   costForTwo,
   avgRatingString,
+  isOpen,
+  veg,
 }) => {
-  console.log(sla);
-
   return (
-    <div className="w-[270px] min-h-[45vh] rounded-sm bg-shadow cursor-pointer hover:scale-105 duration-200 bg-[#373737] m-2 p-2">
+    <div className="w-[270px] min-h-[45vh] rounded-sm bg-shadow cursor-pointer hover:scale-105 duration-200 bg-[#373737] m-2 p-2 relative">
       <img
         className="w-full h-40 rounded-sm text-center"
         src={IMG_CDN_URL + cloudinaryImageId}
       />
-      <h3 className="text-center font-bold text-base text-white">{name}</h3>
+      <h3 className="text-center font-bold text-base text-white bg-[#B22126]">
+        {name}
+      </h3>
       <h5 className="text-teal-500 text-center text-sm">
         {cuisines.join(", ")}
       </h5>
       <h5 className="text-center text-cyan-600 text-sm">{areaName}</h5>
       <span className="flex justify-center gap-2 mt-1">
-        <p className=" text-white text-sm">
+        <p className=" text-white text-xs">
           {sla?.lastMileTravelString ?? "2.0 km"}
         </p>
-        <p className=" text-white text-sm">{costForTwo ?? "₹200 for two"}</p>
-        <p className="text-white">{sla.slaString}</p>
+        <LiaDotCircle className="inline text-xs relative top-[3px] text-[#B22126]" />
+        <p className="text-white text-xs">{sla.slaString}</p>
+        <LiaDotCircle className="inline text-xs relative top-[3px] text-[#B22126]" />
+        <p className=" text-white text-xs">{costForTwo ?? "₹200 for two"}</p>
+      </span>
+      <span className="text-white flex justify-center gap-2 mt-2">
+        {isOpen ? (
+          <p className="bg-green-500 px-1">open</p>
+        ) : (
+          <p className="bg-[#B22126] px-1">closed</p>
+        )}
+        {veg ? (
+          <p className="bg-green-500 px-1">veg</p>
+        ) : (
+          <p className="bg-[#B22126] px-1">nog-veg</p>
+        )}
       </span>
 
-      <div className="flex justify-center items-center mt-2">
+      <div className="flex absolute top-0 right-0 justify-center items-center mt-2">
         {avgRatingString > 4 ? (
           <>
             {avgRatingString > 4.5 ? (
